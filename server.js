107dff668f35e75cd409edcf28b57037ff1a2961
uzair-anwar <uzair.anwar@devsinc.com>
2022-08-15 19:19:32 +0500

    Moved frontend link to env file

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
-    methods: "GET,PUT,PATCH,POST,DELETE",
+    origin: [`${process.env.FRONTEND_URL}`],
     credentials: true,
   })
 );
